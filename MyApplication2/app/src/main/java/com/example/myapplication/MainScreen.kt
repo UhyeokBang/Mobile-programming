bc82f0e69dd47fb851d574d6c20447f8f7b37548
Bang Uhyeok <0105bang@gmail.com>
2024-04-03 12:38:39 +0900

    class 0403

--- MyApplication2/app/src/main/java/com/example/myapplication/MainScreen.kt
+++ MyApplication2/app/src/main/java/com/example/myapplication/MainScreen.kt
@@ -20,7 +20,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.listSaver
+import androidx.compose.runtime.saveable.mapSaver
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -28,8 +31,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.AsyncImage
 import kotlinx.android.parcel.Parcelize
 
@@ -90,17 +96,51 @@ fun IconWithBadge(counter: Int, onClick:() -> Unit){
 @Parcelize
 data class ImgData(var img:Int, var counter:Int) : Parcelable
 
+data class ImgData2(var img:Int, var counter:Int) {
+    companion object{
+        val imgID = "IMGID"
+        val imgCounter = "Counter"
+        val imgMapSaver = mapSaver(
+            save = { mapOf(imgID to it.img, imgCounter to it.counter) },
+            restore = {ImgData2(it[imgID] as Int, it[imgCounter] as Int)}
+        )
+        val imgListSaver = listSaver(
+            save = { listOf(it.img, it.counter)},
+            restore = {ImgData2(it[0] as Int, it[1] as Int)}
+        )
+    }
+}
+
+class ImgViewModel:ViewModel(){
+    var imgList = mutableStateListOf<ImgData2>()
+        private set
+
+    init{
+        imgList.add(ImgData2(R.drawable.image1, 10))
+        imgList.add(ImgData2(R.drawable.image2, 20))
+        imgList.add(ImgData2(R.drawable.image3, 30))
+    }
+
+    fun incrCount(index: Int){
+        imgList[index] = imgList[index].copy(counter = imgList[index].counter+1)
+    }
+
+}
+
 @Composable
-fun MainScreen(){
+fun MainScreen(imgViewModel: ImgViewModel=viewModel()){
+
+    val context = LocalContext.current
+
     val scrollState = rememberScrollState()
-    var img1 by rememberSaveable {
-        mutableStateOf(ImgData(R.drawable.image1, 10))
+    var img1 by rememberSaveable(stateSaver = ImgData2.imgListSaver) {
+        mutableStateOf(ImgData2(R.drawable.image1, 10))
     }
-    var img2 by rememberSaveable {
-        mutableStateOf(ImgData(R.drawable.image2, 20))
+    var img2 by rememberSaveable(stateSaver = ImgData2.imgListSaver) {
+        mutableStateOf(ImgData2(R.drawable.image2, 20))
     }
-    var img3 by rememberSaveable {
-        mutableStateOf(ImgData(R.drawable.image3, 30))
+    var img3 by rememberSaveable(stateSaver = ImgData2.imgListSaver) {
+        mutableStateOf(ImgData2(R.drawable.image3, 30))
     }
     var counter4 by rememberSaveable {
         mutableStateOf(50)
@@ -111,8 +151,10 @@ fun MainScreen(){
         .verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ImageWithSlot(img = img1.img) {
-            ButtonWithIcon(counter = img1.counter, {img1 = img1.copy(counter = img1.counter+1)})
+        ImageWithSlot(img = imgViewModel.imgList[0].img) {
+            ButtonWithIcon(counter = imgViewModel.imgList[0].counter) {
+                imgViewModel.incrCount(0)
+            }
         }
         ImageWithSlot(img = img2.img) {
             ButtonWithIcon(counter = img2.counter, {img2 = img2.copy(counter = img2.counter+1)})
